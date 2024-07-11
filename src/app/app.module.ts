320bfd3298f8b3ded3c025a3ebec8c4605d0b521
Lohran Mendes <lohranfm@gmail.com>
2024-07-11 13:02:10 -0300

    feat: add user-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule,
