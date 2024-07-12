ebbb86c2455ce96b3f6a65c99b72265b6b2cbd05
Lohran Mendes <lohranfm@gmail.com>
2024-07-12 09:26:58 -0300

    feat: add diretiva bidirecional

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-input.component.css']
 })
 export class UserInputComponent {
+  enteredInvestmentInitial: number = 0;
+  enteredInvestmentAnnual: number = 0;
+  enteredInvestmentReturnal: number = 0;
+  enteredInvestmentDuration: number = 0;
 
+onSubmit(){
+  console.log('funcionou, foi submetido')
+  console.log(this.enteredInvestmentInitial)
+  console.log(typeof this.enteredInvestmentInitial)
+}
 }
